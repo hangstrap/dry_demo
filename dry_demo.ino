f87bb62291df3a85c5f2344050ca72d7e8516953
hangstrap <hangstrap@gmail.com>
2024-01-29 20:35:41 +1300

    make value update on change

--- dry_demo.ino
+++ dry_demo.ino
@@ -34,11 +34,35 @@
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 XPT2046_Touchscreen touch(TOUCH_CS);
+class Environment {
+  public:
+    int temperature =0;
+    int humidity = 0;
+    int pressure = 0;
+    Environment( int temp, int hum, int pres){
+      temperature = temp;
+      humidity = hum;
+      pressure = pres;
+    }
+    bool equal( Environment other){
+      if( temperature != other.temperature){
+        return false;
+      }
+      if( humidity != other.humidity){
+        return false;
+      }
+      if( pressure != pressure){
+        return false;
+      }
+      return true;
+    }
+} ;
+
+Environment lastReading(0,0,0);
 
 void setup() {
-   Serial.begin(115200); //Use serial monitor for debugging
+  Serial.begin(115200); //Use serial monitor for debugging
   delay(1000);
-
 
   if (!ENV.begin()) {
     Serial.println("Failed to initialize MKR ENV Shield!");
@@ -54,7 +78,9 @@ void setup() {
   tft.fillScreen(ILI9341_BLACK);
   
   IntroScreen();
-  displayEnv();
+
+  lastReading = readCurrentValue();
+  displayEnv( lastReading);
   digitalWrite(TFT_LED, LOW);    // LOW to turn backlight on; 
 
 
@@ -62,59 +88,70 @@ void setup() {
 
 void loop() {
     delay(1000);
-    displayEnv();
+    Environment currentValue = readCurrentValue();
+    if( !lastReading.equal( currentValue)){
+      lastReading = currentValue;
+      displayEnv( lastReading);
+    }
 }
 
 void IntroScreen()
 {
   //Draw the Result Box
-  tft.fillRect(0, 0, 240, 320, ILI9341_WHITE);
+  tft.fillRect(0, 0, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT, ILI9341_WHITE);
   tft.drawRGBBitmap(50,10,image_data_DryLivingLogo,150,158);
 
   tft.setTextSize(0);
   tft.setTextColor(ILI9341_BLACK);
-  tft.setFont(&FreeSansBold9pt7b);  
+  tft.setFont( &FreeSansBold9pt7b);  
 
   tft.setCursor(42, 190);
   tft.println("Humidity Controller");  
 
 }
-void displayEnv(){
-  int xCursor = 10;
-  int yCursor = 251;
-  int yIncrement = 25;
-  float temperature = ENV.readTemperature();
-  float humidity    = ENV.readHumidity();
-  float pressure    = ENV.readPressure();
+
+Environment readCurrentValue(){
+  return  Environment ( ENV.readTemperature(),  ENV.readHumidity(), ENV.readPressure());
+}
+void printEnv( Environment environment){
 
   Serial.print("Temperature = ");
-  Serial.print(temperature);
+  Serial.print( environment.temperature);
   Serial.println(" °C");
 
   Serial.print("Humidity    = ");
-  Serial.print(humidity);
+  Serial.print( environment.humidity);
   Serial.println(" %");
 
   Serial.print("Pressure    = ");
-  Serial.print(pressure);
+  Serial.print( environment.pressure);
   Serial.println(" kPa");
+}
+
+void displayEnv( Environment environment){
+
+  int xCursor = 10;
+  int yCursor = 250;
+  int yIncrement = 25;
+
+  tft.fillRect(0, yCursor -14, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT-yCursor, ILI9341_ORANGE);
 
   tft.setTextColor(ILI9341_RED);
 
   tft.setCursor(xCursor, yCursor);
   tft.print("Temperature = ");
-  tft.print(temperature);
+  tft.print( environment.temperature);
   tft.println(" °C");
   
   tft.setCursor(xCursor, yCursor += yIncrement);
   tft.print("Humidity    = ");
-  tft.print(humidity);
+  tft.print( environment.humidity);
   tft.println(" %");
 
 
   tft.setCursor(xCursor, yCursor += yIncrement);
   tft.print("Pressure    = ");
-  tft.print(pressure);
+  tft.print( environment.pressure);
   tft.println(" kPa");
 
 }
